505c09864fdd773ac516ebe594e1f04f317d3e8b
ammwangi <elijah.mwangi@kamsware.com>
2022-07-25 14:28:14 +0300

    Add project files.

---
+++ WikipediaCommentAnalysis/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using static Microsoft.ML.DataOperationsCatalog;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "yelp_labelled.txt");
+
+        MLContext mlContext = new MLContext();
+
+        TrainTestData splitDataView = LoadData(mlContext);
+
+
+
+
+        TrainTestData LoadData(MLContext mlContext)
+        {
+            IDataView dataView = mlContext.Data.LoadFromTextFile<SentimentData>(_dataPath, hasHeader: false);
+            TrainTestData splitDataView = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+            return splitDataView;
+        }
+
+        ITransformer model = BuildAndTrainModel(mlContext, splitDataView.TrainSet);
+
+
+        ITransformer BuildAndTrainModel(MLContext mlContext, IDataView splitTrainSet)
+        {
+            var estimator = mlContext.Transforms.Text.FeaturizeText(outputColumnName: "Features", inputColumnName: nameof(SentimentData.SentimentText))
+                .Append(mlContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features"));
+            Console.WriteLine("=============== Create and Train the Model ===============");
+            var model = estimator.Fit(splitTrainSet);
+            Console.WriteLine("=============== End of training ===============");
+            Console.WriteLine();
+            return model;
+        }
+
+        Evaluate(mlContext, model, splitDataView.TestSet);
+
+        void Evaluate(MLContext mlContext, ITransformer model, IDataView splitTestSet)
+        {
+            Console.WriteLine("=============== Evaluating Model accuracy with Test data===============");
+            IDataView predictions = model.Transform(splitTestSet);
+            CalibratedBinaryClassificationMetrics metrics = mlContext.BinaryClassification.Evaluate(predictions, "Label");
+        }
+    }
+}
+
+public class SentimentData
+{
+    [LoadColumn(0)]
+    public string SentimentText;
+
+    [LoadColumn(1), ColumnName("Label")]
+    public bool Sentiment;
+}
+
+public class SentimentPrediction : SentimentData
+{
+
+    [ColumnName("PredictedLabel")]
+    public bool Prediction { get; set; }
+
+    public float Probability { get; set; }
+
+    public float Score { get; set; }
+}
---
+++ bmptopngconvert/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Drawing;
+using System.Drawing.Imaging;
+
+Console.WriteLine("Hello, World!");
+const string SOURCE_PATH = @"D:\Machine-learning\MachineLearning-Images\positive\Averages";
+var files = Directory.GetFiles(SOURCE_PATH, "*.*", SearchOption.AllDirectories).ToList();
+
+foreach (var filename in files)
+{
+    if (filename.Contains(".bmp"))
+    {
+        var indeoftxt = filename.IndexOf(".txt.");
+        var startindex = indeoftxt - 17;
+        var newPngName = filename.Substring(startindex, 21 - 4);
+        //"D:\\Machine-learning\\MachineLearning-Images\\negative\\Averages\\mBvh____\\19-01-08-12-04-14.txt.bmp"
+
+        //23 => 101
+
+        string pathnew = filename.Substring(0, filename.Length - 25);
+        var imaged = Image.FromFile(filename);
+        imaged.Save(pathnew + newPngName + ".png", ImageFormat.Png);
+        imaged.Dispose();
+        System.IO.File.Delete(filename);
+
+    }
+
+    //using (var bitmap = new Bitmap(filename))
+    //using (var stream = new MemoryStream())
+    //{
+    //    bitmap.Save(stream, ImageFormat.Png);
+    //}
+
+}
